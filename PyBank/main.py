2f9c631cb5af407bf12f2a80c363e58d365c3520
Katie <katie@dhcp-10-101-89-82.wireless.northwestern.private>
2019-03-07 17:46:15 -0600

    First commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,41 @@
-PyBank main.py
+import os
+import csv
+Total_Margin = 0
+Total_Months = 0
+Previous_Value = 0
+Changes = []
+date = []
+
+
+budget_data = os.path.join('budget_data.csv')
+with open(budget_data, newline="") as csvfile:
+   csvreader = csv.reader(csvfile, delimiter=",")
+   csv_header = next(csvreader)
+   for row in csvreader:
+    Total_Months += 1
+    Total_Margin += int(row[1])
+    Current_Value = row[1]
+    change = (int(Current_Value) - int(Previous_Value))
+    Changes.append(change)
+    date.append(row[0])
+    Previous_Value = row[1]
+Changes=Changes[1:]
+max_changes = (max(Changes), str(date[Changes.index(max(Changes))+1]))
+min_changes = (min(Changes), str(date[Changes.index(min(Changes))+1]))
+
+print("Financial Analysis")
+print("-----------------------------------------------")
+print("Total Months:", Total_Months) 
+print('Total Margin: ${:,}'.format(Total_Margin))
+print('Avg Change: ${:,}'.format(round(sum(Changes)/len(Changes),2)))
+print('Greatest Revenue Increase: ${:,}'.format(max(Changes)) + " - " + str(date[Changes.index(max(Changes))+1]))
+print('Greatest Revenue Decrease: ${:,}'.format(min(Changes)) + " - " + str(date[Changes.index(min(Changes))+1]))
+print("-----------------------------------------------")
+
+Final_Analysis = (Total_Months, Total_Margin, round(sum(Changes)/len(Changes),2), str(max(Changes)) + str(date[Changes.index(max(Changes))+1]), str(min(Changes)) + str(date[Changes.index(min(Changes))+1]))
+
+output_file = os.path.join("pybank_output.txt")
+with open(output_file, 'w') as text_file:
+    writer = csv.writer(text_file)
+    writer.writerow(Final_Analysis)
+    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,67 @@
-PyPoll main.py
+import os
+import csv
+import collections
+
+poll_data = os.path.join("..", "PyPoll", "election_data.csv")
+
+with open(poll_data, newline="") as csvfile:
+    electionresults = csv.reader(csvfile, delimiter=",")
+    next(electionresults, None)
+    
+    Total_Votes = 0
+    Khan = 0
+    Correy = 0
+    Li = 0
+    OTooley = 0
+
+    for record in electionresults:
+
+        Total_Votes +=1
+        if record[2] == "Khan":
+            Khan += 1
+        elif record[2] == "Correy":
+            Correy += 1
+        elif record[2] == "Li":
+            Li += 1
+        elif record[2] == "O'Tooley":
+            OTooley +=1 
+            pass
+        
+    Khan_pct = round(Khan/Total_Votes*100)
+    Correy_pct = round(Correy/Total_Votes*100)
+    Li_pct = round(Li/Total_Votes*100)
+    OTooley_pct = round(OTooley/Total_Votes*100)    
+
+
+    
+    print("Election Results")
+    print("---------------------")
+    print("Total Votes:" + str(Total_Votes))
+    print("---------------------")
+    print ("Khan: " + str(Khan_pct) +"%  (" + str(Khan) + ")")
+    print("Correy: " + str(Correy_pct) +"%  (" + str(Correy) +")") 
+    print("Li: " + str(Li_pct) + "%  (" + str(Li) + ")")
+    print("O'Tooley: " + str(OTooley_pct) + "% (" + str(OTooley) + ")")
+    print("-----------------------")
+    print("Winner: Khan")
+    print("-----------------------")
+ 
+
+output_file = os.path.join("pypoll_output.txt")
+with open(output_file, 'w') as text_file:
+    writer = csv.writer(text_file)
+    writer.writerow("Election Results")
+    writer.writerow("---------------------")
+    writer.writerow("Total Votes:" + str(Total_Votes))
+    writer.writerow("---------------------")
+    writer.writerow("Khan: " + str(Khan_pct) +"%  (" + str(Khan) + ")")
+    writer.writerow("Correy: " + str(Correy_pct) +"%  (" + str(Correy) +")") 
+    writer.writerow("Li: " + str(Li_pct) + "%  (" + str(Li) + ")")
+    writer.writerow("O'Tooley: " + str(OTooley_pct) + "% (" + str(OTooley) + ")")
+    writer.writerow("-----------------------")
+    writer.writerow("Winner: Khan")
+    writer.writerow("-----------------------")
+    
+   
+
+
